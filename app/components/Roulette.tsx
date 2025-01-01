8da5bfaf0dd2df8df5f2e699509df34aec0cb841
snrnapa <shino.satoru@gmail.com>
2025-01-01 20:16:28 +0900

    一応動いている時点

--- app/components/Roulette.tsx
+++ app/components/Roulette.tsx
@@ -18,8 +18,8 @@ const Roulette = () => {
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const screenHeight = typeof window !== "undefined" ? window.innerHeight : 960;
-  const itemHeight = 60; // 1アイテムの高さ
-  const maxScroll = itemHeight * items.length - screenHeight; // 最大スクロール範囲
+  const ITEM_HEIGHT = 60; // 1アイテムの高さ
+  const maxScroll = ITEM_HEIGHT * items.length - screenHeight; // 最大スクロール範囲
 
   // トグル関数
   const toggle = () => {
@@ -34,18 +34,16 @@ const Roulette = () => {
   // ルーレットを動かす関数
   const move = () => {
     if (intervalRef.current) return; // 二重実行防止
+
     intervalRef.current = setInterval(() => {
       setPositionY((prev) => {
-        const nextPosition = prev + itemHeight;
-        if (nextPosition >= maxScroll) {
-          setCurrentIndex((prevIndex) =>
-            prevIndex >= items.length - 1 ? 0 : prevIndex + 1
-          );
-          return 0; // 最大スクロールを越えないようにリセット
+        const nextPosition = prev + ITEM_HEIGHT;
+        if (nextPosition >= items.length * ITEM_HEIGHT) {
+          return 0;
         }
         return nextPosition;
       });
-    }, 16); // 速度調整
+    }, 50); // 更新間隔
   };
 
   // ルーレットを停止する関数
@@ -53,17 +51,25 @@ const Roulette = () => {
     if (intervalRef.current) {
       clearInterval(intervalRef.current); // タイマー停止
       intervalRef.current = null; // リファレンスをリセット
-      setSelectedIndex(currentIndex); // 停止した時点で選ばれたインデックスを保存
+      const closesIntex = Math.round(positionY / ITEM_HEIGHT) % items.length;
+      setSelectedIndex(closesIntex); // 停止した時点で選ばれたインデックスを保存
+      console.log(closesIntex);
     }
   };
 
   return (
     <div
       onClick={toggle}
-      className="flex justify-center  items-center min-h-screen bg-gradient-to-b from-blue-900 via-blue-800 to-blue-950"
+      className="flex justify-center  items-center min-h-screen bg-gradient-to-b from-blue-900 via-blue-800 to-blue-950 "
     >
-      <div className="h-full w-full flex flex-col   justify-center items-center  ">
-        <div className="flex flex-col space-y-4 ">
+      <div className="h-8 w-full flex flex-col   justify-center items-center  ">
+        <div
+          className="  space-y-28"
+          style={{
+            transform: `translateY(-${positionY}px)`,
+            transition: isRunning ? "transform 0.1s linear" : "none",
+          }}
+        >
           {items.map((item, index) => (
             <RouletteText
               key={index}
